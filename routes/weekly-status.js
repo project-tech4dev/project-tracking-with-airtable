0018ab6bacdd53317b6536bd1671df69e2366636
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2019-09-24 01:20:57 +0530

    added code comments

--- routes/weekly-status.js
+++ routes/weekly-status.js
@@ -27,10 +27,24 @@ router.get('', (req, res, next) => {
 });
 
 router.get('/fridayreminder', (req, res, next) => {
+    // fetch all the active projects
+
+    // find the partner poc for active projects
+
+    // send a reminder email to add weekly status activity
+
     res.status(200).end("Friday reminder sent successfuly!");
 });
 
 router.get('/mondayreminder', (req, res, next) => {
+    // fetch all the active projects
+
+    // find the partner poc for active projects
+
+    // check for active projects that does not have weekly status activity since Friday - Monday
+
+    // send second reminder email to above project partner POCS
+
     res.status(200).end("Monday reminder sent successfuly!");
 });
 

9a3c5426eaf60beef314941805f4c8025d929e0c
Priyanka Shirude <priyanka.shirude@webaccessglobal.com>
2020-01-23 14:47:12 +0530

    changes to convert USD to INR rates

--- routes/report.js
+++ routes/report.js
@@ -24,8 +24,13 @@ router.get("", (req, res, next) => {
             <h2>Cohort Wise Report</h2>
             <ul>
                 <li>
-                    <a href='/report/firstCohort'>
+                    <a href='/report/firstCohort?cohortType=firstCohort'>
                         Download First Cohort Report
+                    </a>
+                </li>
+                <li>
+                    <a href='/report/secondCohort?cohortType=secondCohort'>
+                        Download Second Cohort Report
                     </a>
                 </li>
             </ul>
@@ -36,229 +41,248 @@ router.get("", (req, res, next) => {
 router.get("/firstCohort", (req, res, next) => {
   // fetch all the active projects
   var firstCohortProjects = [];
-  var startDateString = req.query.startDate;
-  var endDateString = req.query.endDate;
+  var cohortType = req.query.cohortType;
+  //var cohortType = "firstCohort";
+  // const cohortType = request.getHeader('');
   // if(startDate > endDate){
   //     res.status(200).end("<span style='color:red'>Improper date range</span>");
   // }
+  // console.log(cohortType);
+  var cohortId = getCohortTypeId(cohortType);
+  // console.log(cohortId);
   var Airtable = require("airtable");
   var base = new Airtable({
     apiKey: process.env.AIRTABLE_API_KEY
   }).base("appfO9PMTzzFk9466");
-  base("Projects")
-    .select({
-      // Selecting the first 25 records in Main View:
-      view: "Cohort wise report (Do not modify)"
-    })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-        records.forEach(function (record) {
-          var projectId = record.id;
-          var ngoName = record.get("NGO Name");
-          var ngoSector = record.get("NGO Sector");
-          var ngoMission = record.get("NGO Mission");
-          var spName = record.get("SP Name");
-          var description = record.get("Description");
-          var estimatedWeeks = record.get("Estimated Weeks");
-          var weeksSpent = record.get("Weeks Spent");
-          var status = record.get("Status");
-          var grade = record.get("Grade");
-          var inrProjectCost = "";
-          var usdProjectCost = record.get("Project Cost");
-          var toolsUsed = record.get("Tools Used");
-          var startDate = record.get("Start Date");
-          var endDate = record.get("End Date");
-          var handoverDate = record.get("Handover Date");
-          if (ngoName) {
-            if (usdProjectCost) {
-              inrProjectCost = Math.round(usdProjectCost * USDtoINRConversionRate);
+  base('Exchange rates').find(cohortId, function (err, record) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    USDtoINRConversionRate = record.get('Rs 1 USD = x Rs');
+    // console.log("amt",USDtoINRConversionRate);
+    base("Projects")
+      .select({
+        // Selecting the first 25 records in Main View:
+        view: "Cohort wise report (Do not modify)"
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+          records.forEach(function (record) {
+            var projectId = record.id;
+            var ngoName = record.get("NGO Name");
+            var ngoSector = record.get("NGO Sector");
+            var ngoMission = record.get("NGO Mission");
+            var spName = record.get("SP Name");
+            var description = record.get("Description");
+            var estimatedWeeks = record.get("Estimated Weeks");
+            var weeksSpent = record.get("Weeks Spent");
+            var status = record.get("Status");
+            var grade = record.get("Grade");
+            var inrProjectCost = "";
+            var usdProjectCost = record.get("Invoiced Cost");
+            var toolsUsed = record.get("Tools Used");
+            var startDate = record.get("Start Date");
+            var endDate = record.get("End Date");
+            var handoverDate = record.get("Handover Date");
+            if (ngoName) {
+              if (usdProjectCost) {
+                inrProjectCost = Math.round(usdProjectCost * USDtoINRConversionRate);
+              }
+              firstCohortProjects.push({
+                projectId: projectId,
+                ngoName: ngoName[0],
+                ngoSector: ngoSector[0],
+                ngoMission: ngoMission[0],
+                spName: spName[0],
+                description: description,
+                estimatedWeeks: estimatedWeeks,
+                weeksSpent: weeksSpent,
+                status: status,
+                grade: grade,
+                inrProjectCost:
+                  inrProjectCost != undefined
+                    ? formatterINR.format(inrProjectCost)
+                    : "",
+                usdProjectCost:
+                  usdProjectCost != undefined
+                    ? formatterUSD.format(usdProjectCost)
+                    : "",
+                toolsUsed: toolsUsed,
+                completed: "",
+                comments: "",
+                blog: "",
+                startDate: startDate,
+                endDate: endDate,
+                handoverDate: handoverDate
+              });
             }
+          });
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          } else {
+            var ngoCount = 0;
+            var tempNgoCount = 0;
+            firstCohortProjects.forEach(function (firstCohortProject, index) {
+              ngoCount++;
+              var projectID = firstCohortProject["projectId"];
+              var comment = "";
+              var percentCompleted = "";
+              var hoursSpent = 0;
+              var linkToBlog = "";
 
-            firstCohortProjects.push({
-              projectId: projectId,
-              ngoName: ngoName[0],
-              ngoSector: ngoSector[0],
-              ngoMission: ngoMission[0],
-              spName: spName[0],
-              description: description,
-              estimatedWeeks: estimatedWeeks,
-              weeksSpent: weeksSpent,
-              status: status,
-              grade: grade,
-              inrProjectCost:
-                inrProjectCost != undefined
-                  ? formatterINR.format(inrProjectCost)
-                  : "",
-              usdProjectCost:
-                usdProjectCost != undefined
-                  ? formatterUSD.format(usdProjectCost)
-                  : "",
-              toolsUsed: toolsUsed,
-              completed: "",
-              comments: "",
-              blog: "",
-              startDate: startDate,
-              endDate: endDate,
-              handoverDate: handoverDate
-            });
-          }
-        });
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          console.error(err);
-          return;
-        } else {
-          var ngoCount = 0;
-          var tempNgoCount = 0;
-          firstCohortProjects.forEach(function (firstCohortProject, index) {
-            ngoCount++;
-            var projectID = firstCohortProject["projectId"];
-            var comment = "";
-            var percentCompleted = "";
-            var hoursSpent = 0;
-            var linkToBlog = "";
-
-            base("Activity")
-              .select({
-                view: "Cohort Project Status (Do not modify)"
-              })
-              .eachPage(
-                function page(records, fetchNextPage) {
-                  // This function (`page`) will get called for each page of records.
-                  records.forEach(function (record) {
-                    var project = record.get("Project");
-                    if (projectID == project) {
-                      comment += record.get("Comments");
-                      if (comment != "") {
-                        comment += ", ";
-                      }
-                      hoursSpent += record.get("Hours spent");
-                      percentCompleted = record.get("% Completed");
-                      if (record.get("Link to Blog") != undefined) {
-                        linkToBlog += record.get("Link to Blog");
-                        if (linkToBlog != "") {
-                          linkToBlog += ", ";
+              base("Activity")
+                .select({
+                  view: "Cohort Project Status (Do not modify)"
+                })
+                .eachPage(
+                  function page(records, fetchNextPage) {
+                    // This function (`page`) will get called for each page of records.
+                    records.forEach(function (record) {
+                      var project = record.get("Project");
+                      if (projectID == project) {
+                        comment += record.get("Comments");
+                        if (comment != "") {
+                          comment += ", ";
                         }
-                      }
-                    }
-                  });
-                  fetchNextPage();
-                },
-                function done(err) {
-                  if (err) {
-                    console.error(err);
-                    return;
-                  } else {
-                    tempNgoCount++;
-                    firstCohortProject["completed"] = percentCompleted;
-                    firstCohortProject["comments"] = comment.replace(
-                      /,\s*$/,
-                      ""
-                    );
-                    firstCohortProject["weeksSpent"] =
-                      hoursSpent != 0 ? (hoursSpent / 40).toFixed(2) : "0";
-                    firstCohortProject["blog"] = linkToBlog.replace(
-                      /,\s*$/,
-                      ""
-                    );
-
-                    if (ngoCount == tempNgoCount) {
-                      const workbook = new Excel.Workbook();
-                      const worksheet = workbook.addWorksheet("First Cohort");
-
-                      // add column headers
-                      worksheet.columns = [
-                        {
-                          header: "NGO Name",
-                          key: "ngoName",
-                          width: 20,
-                          bold: true
-                        },
-                        { header: "NGO Sector", key: "ngoSector", width: 20 },
-                        { header: "NGO Mission", key: "ngoMission", width: 20 },
-                        { header: "SP Name", key: "spName", width: 20 },
-                        {
-                          header: "Project Description",
-                          key: "description",
-                          width: 40
-                        },
-                        {
-                          header: "Estimated Weeks",
-                          key: "estimatedWeeks",
-                          width: 20
-                        },
-                        { header: "Weeks Spent", key: "weeksSpent", width: 20 },
-                        { header: "Project Stage", key: "status", width: 20 },
-                        { header: "% Completed", key: "completed", width: 20 },
-                        { header: "Grade", key: "grade", width: 20 },
-                        {
-                          header: "Project Cost (USD)",
-                          key: "usdProjectCost",
-                          width: 20
-                        },
-                        {
-                          header: "Project Cost (INR)",
-                          key: "inrProjectCost",
-                          width: 20
-                        },                        
-                        { header: "Tools Used", key: "toolsUsed", width: 20 },
-                        { header: "Comments", key: "comments", width: 80 },
-                        { header: "Link to blog", key: "blog", width: 80 },
-                        { header: "Start Date", key: "startDate", width: 20 },
-                        { header: "End Date", key: "endDate", width: 20 },
-                        { header: "Handover Date", key: "handoverDate", width: 20 }
-                        
-                      ];
-
-                      var i = 0;
-
-                      worksheet.addRows(firstCohortProjects);
-                      var rowCount = worksheet.rowCount;
-                      for (i = 1; i <= rowCount; i++) {
-                        let row = worksheet.getRow(i);
-                        if (row === null || !row.values || !row.values.length)
-                          return [];
-
-                        row.alignment = {
-                          wrapText: true,
-                          vertical: "middle",
-                          horizontal: "center"
-                        };
-                        if (i == 1) {
-                          for (j = 1; j <= row.values.length; j++) {
-                            row.getCell(j).font = { bold: true };
+                        hoursSpent += record.get("Hours spent");
+                        percentCompleted = record.get("% Completed");
+                        if (record.get("Link to Blog") != undefined) {
+                          linkToBlog += record.get("Link to Blog");
+                          if (linkToBlog != "") {
+                            linkToBlog += ", ";
                           }
                         }
                       }
-
-                      var fileName = "First_Cohort_Report.xlsx";
-
-                      res.setHeader(
-                        "Content-Type",
-                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                    });
+                    fetchNextPage();
+                  },
+                  function done(err) {
+                    if (err) {
+                      console.error(err);
+                      return;
+                    } else {
+                      tempNgoCount++;
+                      firstCohortProject["completed"] = percentCompleted;
+                      firstCohortProject["comments"] = comment.replace(
+                        /,\s*$/,
+                        ""
                       );
-                      res.setHeader(
-                        "Content-Disposition",
-                        "attachment; filename=" + fileName
+                      firstCohortProject["weeksSpent"] =
+                        hoursSpent != 0 ? (hoursSpent / 40).toFixed(2) : "0";
+                      firstCohortProject["blog"] = linkToBlog.replace(
+                        /,\s*$/,
+                        ""
                       );
 
-                      workbook.xlsx.write(res).then(function () {
-                        res.end();
-                      });
+                      if (ngoCount == tempNgoCount) {
+                        const workbook = new Excel.Workbook();
+                        const worksheet = workbook.addWorksheet("First Cohort");
+
+                        // add column headers
+                        worksheet.columns = [
+                          {
+                            header: "NGO Name",
+                            key: "ngoName",
+                            width: 20,
+                            bold: true
+                          },
+                          { header: "NGO Sector", key: "ngoSector", width: 20 },
+                          { header: "NGO Mission", key: "ngoMission", width: 20 },
+                          { header: "SP Name", key: "spName", width: 20 },
+                          {
+                            header: "Project Description",
+                            key: "description",
+                            width: 40
+                          },
+                          {
+                            header: "Estimated Weeks",
+                            key: "estimatedWeeks",
+                            width: 20
+                          },
+                          { header: "Weeks Spent", key: "weeksSpent", width: 20 },
+                          { header: "Project Stage", key: "status", width: 20 },
+                          { header: "% Completed", key: "completed", width: 20 },
+                          { header: "Grade", key: "grade", width: 20 },
+                          {
+                            header: "Invoice Cost (USD)",
+                            key: "usdProjectCost",
+                            width: 20
+                          },
+                          {
+                            header: "Invoice Cost (INR)",
+                            key: "inrProjectCost",
+                            width: 20
+                          },
+                          { header: "Tools Used", key: "toolsUsed", width: 20 },
+                          { header: "Comments", key: "comments", width: 80 },
+                          { header: "Link to blog", key: "blog", width: 80 },
+                          { header: "Start Date", key: "startDate", width: 20 },
+                          { header: "End Date", key: "endDate", width: 20 },
+                          { header: "Handover Date", key: "handoverDate", width: 20 }
+
+                        ];
+
+                        var i = 0;
+
+                        worksheet.addRows(firstCohortProjects);
+                        var rowCount = worksheet.rowCount;
+                        for (i = 1; i <= rowCount; i++) {
+                          let row = worksheet.getRow(i);
+                          if (row === null || !row.values || !row.values.length)
+                            return [];
+
+                          row.alignment = {
+                            wrapText: true,
+                            vertical: "middle",
+                            horizontal: "center"
+                          };
+                          if (i == 1) {
+                            for (j = 1; j <= row.values.length; j++) {
+                              row.getCell(j).font = { bold: true };
+                            }
+                          }
+                        }
+
+                        var fileName = "First_Cohort_Report.xlsx";
+
+                        res.setHeader(
+                          "Content-Type",
+                          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        );
+                        res.setHeader(
+                          "Content-Disposition",
+                          "attachment; filename=" + fileName
+                        );
+
+                        workbook.xlsx.write(res).then(function () {
+                          res.end();
+                        });
+                      }
                     }
                   }
-                }
-              );
-          });
+                );
+            });
+          }
         }
-      }
-    );
+      );
+  });
 });
+
+function getCohortTypeId(cohortType) {
+  if (cohortType == "firstCohort") {
+    return "recMXqWU8Ts4j7zRZ";
+  } else if (cohortType == "secondCohort") {
+    return "recvK1HBfnlSMgAvR";
+  }
+}
 
 module.exports = router;
